2375b8782628585ba046180bbb9931484410d03c
Ayush Rawat <ayushrawat11562@gmail.com>
2022-04-21 19:36:15 +0530

    News fetched n displayed

--- src/components/News.js
+++ src/components/News.js
@@ -1,24 +1,71 @@
-import {useEffect} from 'react';
-import {options} from './api/newsapi'
+import {useEffect, useState} from 'react';
+import {options} from './api/newsapi';
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const News = ()=> {
+
+    const [loading, setLoading] = useState(false);
+    const [news, setNews] = useState([]);
+    const [errorMsg, setErrorMsg] = useState('');
+    const [page, setPage] = useState(0);
+
+    const url = 'https://crypto-news-live3.p.rapidapi.com/news';
+    const loadMore = () => {
+        // setLoading(!loading);
+    };
+
     useEffect(() => {
-        const url = 'https://crypto-news-live3.p.rapidapi.com/news'
+
         const fetchData = async () => {
             
-              const response = await fetch(url,options);
-              const json = await response.json();
-              console.log(json);
+            fetch(url, options)
+            .then(response => response.json())
+            .then(function (result) {
+                const arr=[];
+                for (var i=0;i<result.length;i++){
+                    arr.push(result[i]);
+                }
+                console.log(arr);
+                setNews(arr);
+            })
+            .catch(err => console.error(err));
             }
-        
-      
-          fetchData();
-    }, []);
-    
+            
+            fetchData();
+    },[]);
     return (
       <div>
-          <h1>News</h1>
+          {errorMsg && <p>This is error function</p>}
+        <Row style={{margin:'10px'}} xs={1} md={2} lg={3}>
+          
+        {news.map((ele,ind) => (
+            // <li key={ind} >{ele.title}</li>
+            // <ListGroup.Item key={ind}>{ele.title}</ListGroup.Item>
+            <Col >
+            <Card key={ind}>
+                
+                <Card.Body>
+                <Card.Text>{ele.title}</Card.Text>
+                <Card.Link href={ele.url}>Visit site</Card.Link>
+                {/* <Card.Link href={ele.url}>News Source</Card.Link> */}
+                <br />
+                <cite title="Source Title"> source - {ele.source}</cite>
+                </Card.Body>
+                
+            </Card>
+            </Col>
+            ))}
+        </Row>
+            
+            <button onClick={loadMore(page)}>{loading ? 'Loading' : 'Load More'}</button>
       </div>
     )
   }
+
   export {News};
+
+
+  
