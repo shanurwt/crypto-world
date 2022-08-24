862d10fb0e79ed273b2870fc9de988ec326072e3
Ayush Rawat <ayushrawat11562@gmail.com>
2022-08-24 22:18:22 +0530

    perfect build

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,31 +8,31 @@ import '../assets/css/Cards.css'
 function Cards() {
   return (
     <div className="cards">
-            <Card sx={{ minWidth: 400, minHeight: 450, maxWidth: 430, backgroundColor: "#252629" }} className='first'>
-                <CardContent >
-                    <Typography sx={{ fontSize: 30, color:"aliceblue" }} align="center" color="text.secondary" gutterBottom>
+        <Card sx={{ minWidth: 400, minHeight: 450, maxWidth: 430, backgroundColor: "#252629" }} className='first'>
+            <CardContent >
+                <Typography sx={{ fontSize: 30, color:"aliceblue" }} align="center" color="text.secondary" gutterBottom>
                     Today's Top 5 Cryptos 
-                    </Typography>
-                    <TopCryptos/>
-                </CardContent>
-            </Card>
-            <Card sx={{ minWidth: 400, minHeight: 450, maxWidth: 430, backgroundColor: "#252629" }} className='second'>
+                </Typography>
+                <TopCryptos/>
+            </CardContent>
+        </Card>
+        <Card sx={{ minWidth: 400, minHeight: 450, maxWidth: 430, backgroundColor: "#252629" }} className='second'>
             <CardContent>
                 <Typography sx={{ fontSize: 30, color:"aliceblue" }} align="center" color="text.secondary" gutterBottom>
-                Volume Gainers in 24Hrs
+                    Volume Gainers in 24Hrs
                 </Typography>
                 <VolumeInc/>
             </CardContent>
-            </Card>
-            <Card sx={{ minWidth: 400, minHeight: 450, maxWidth: 430, backgroundColor: "#252629" }} className='third'>
+        </Card>
+        <Card sx={{ minWidth: 400, minHeight: 450, maxWidth: 430, backgroundColor: "#252629" }} className='third'>
             <CardContent>
                 <Typography sx={{ fontSize: 30, color:"aliceblue" }} align="center" color="text.secondary" gutterBottom>
-                Volume Losers in 24 Hrs
+                    Volume Losers in 24 Hrs
                 </Typography>
                 <VolumeDec/>
             </CardContent>
-            </Card>
-        </div>
+        </Card>
+    </div>
   )
 }
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,7 @@
 import React, { useRef, useLayoutEffect } from 'react'
 import '../assets/css/Welcome.css'
 import {gsap} from 'gsap';
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import * as ScrollTrigger from "gsap/ScrollTrigger";
 
 function Welcome() {
     const crypto = useRef();
@@ -24,12 +24,12 @@ function Welcome() {
         {
           opacity: 1,
           duration: 3,
-          // y: -100,
+          y: -100,
           ease:"easeIn",
           scrollTrigger: {
             trigger: crypto.current,
-            start: "350 center",
-            end: "600 center",
+            start: "125 center",
+            end: "350 center",
             // markers:true,
             pin:true,
             toggleActions: "play reset resume reset",
@@ -51,8 +51,8 @@ function Welcome() {
           ease:"easeIn",
           scrollTrigger: {
             trigger: cdef.current,
-            start: "100 center",
-            end: "350 center",
+            start: "top center",
+            end: "280 center",
             // markers:true,
             pin:true,
             toggleActions: "play reset resume reset",
@@ -73,8 +73,8 @@ function Welcome() {
           ease:"easeIn",
           scrollTrigger: {
             trigger: ser.current,
-            start: "0 center",
-            end: "250 center",
+            start: "-80 center",
+            end: "100 center",
             // markers:true,
             pin:true,
             toggleActions: "play reset resume reset",
@@ -96,7 +96,7 @@ function Welcome() {
           scrollTrigger: {
             trigger: serdef.current,
             start: "0 center",
-            end: "400 center",
+            end: "300 center",
             // markers:true,
             pin:true,
             toggleActions: "play reset resume reset",
@@ -107,31 +107,35 @@ function Welcome() {
     
   return (
     <div  className='wel_container'>
-        <div >
-          <h1 ref={crypto} className='ques1'>
-          What is Crypto?
+      <div >
+        <h1 ref={crypto} className='ques1'>
+        What is Crypto?
+        </h1>
+      </div>
+
+      <div ref={cdef} className='cdef_div'>
+        <p className='defination'>
+        A crypto or cryptocurrency is a digital or virtual currency that is secured by cryptography, which makes it nearly impossible to counterfeit or double-spend. Many cryptocurrencies are decentralized networks based on blockchain technology—a distributed ledger enforced by a disparate network of computers. A defining feature of cryptocurrencies is that they are generally not issued by any central authority, rendering them theoretically immune to government interference or manipulation.
+        </p>
+      </div>
+
+      <div>
+        <h1 ref={ser} className='ques2'>
+          What we provide?
+        </h1>
+
+        <p ref={serdef} className='defination2'>
+          We provide you the latest news about the crypto and the trending crypto's, volume gainers and losers in 24hrs, and a lot more, percentage changed in crypto and you can also search your crypto here. 
+          The news is provided through crypto-news-live3 API and the everything rest is provided by coingecko API.
+        </p>
+
+      </div>
+        <div>
+          <h1 className='trend'>
+            Today's Trend
           </h1>
         </div>
-        <div ref={cdef} >
-          <p  className='defination'>
-          A crypto or cryptocurrency is a digital or virtual currency that is secured by cryptography, which makes it nearly impossible to counterfeit or double-spend. Many cryptocurrencies are decentralized networks based on blockchain technology—a distributed ledger enforced by a disparate network of computers. A defining feature of cryptocurrencies is that they are generally not issued by any central authority, rendering them theoretically immune to government interference or manipulation.
-          </p>
-          </div>
-        <div>
-            <h1 ref={ser} className='ques2'>
-                What we provide?
-            </h1>
-            <p ref={serdef} className='defination'>
-            We provide you the latest news about the crypto and the trending crypto's, volume gainers and losers in 24hrs, and a lot more, percentage changed in crypto and you can also search your crypto here. 
-            The news is provided through crypto-news-live3 API and the everything rest is provided by coingecko API.
-            </p>
-        </div>
-        <div >
-            <h1 className='trend'>
-                Today's Trend
-            </h1>
-        </div>
-        </div>
+    </div>
   )
 }
 
