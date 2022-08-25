3d4d7afa09c81fede12ca5705e7169081353ec8c
Ayush Rawat <ayushrawat11562@gmail.com>
2022-08-25 11:18:00 +0530

    github link added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div style={{backgroundColor: "#252629", paddingBottom:10}}> 
-       <a style={{backgroundColor: "#252629", textDecoration: "none", color: "white", marginLeft:600,}} href="#lol"> 
+       <a style={{backgroundColor: "#252629", textDecoration: "none", color: "white", marginLeft:600,}} href="https://github.com/shanurwt/crypto-world.git"> 
        Click this github link for source code ©.</a>     
     </div>
   )
